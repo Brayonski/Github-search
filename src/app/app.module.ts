e60f5b6819a6af1fa4719538aed8ea379a32749b
root <root@localhost.localdomain>
2018-08-17 12:26:24 +0300

    added component Github-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubProfileComponent
   ],
   imports: [
     BrowserModule
